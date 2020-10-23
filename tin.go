1759a11ac44892a42713d5fefb369714945b3151
hanjin <hanjin@w-ew.com>
2020-10-23 17:38:19 +0800

    update

--- tin.go
+++ tin.go
@@ -32,6 +32,8 @@ func (tm *TileMaker) GenTile(tsf [6]float64, x, y int) (*Mesh, error) {
 	tileBBox[1] = tsf[3] + tsf[5]*float64(y)
 	tileBBox[3] = tsf[0] + tsf[1]*float64(x)
 	tileBBox[4] = tsf[3]
+	ctx := (tileBBox[0] + tileBBox[3]) / 2
+	cty := (tileBBox[1] + tileBBox[4]) / 2
 
 	for t := range vertsInTile {
 		v := &vertsInTile[t]
@@ -41,21 +43,20 @@ func (tm *TileMaker) GenTile(tsf [6]float64, x, y int) (*Mesh, error) {
 		if v[2] > tileBBox[5] {
 			tileBBox[5] = v[2]
 		}
-		v[0] = (v[0] - tileBBox[0])
-		v[1] = (v[1] - tileBBox[1])
-		v[2] = (v[2] - tileBBox[2])
+		v[0] = (v[0] - ctx)
+		v[1] = (v[1] - cty)
 	}
 
 	fInTile := make([]Face, len(tm.mesh.Faces))
 	copy(fInTile, tm.mesh.Faces)
 	tileMesh := new(Mesh)
 	tileMesh.initFromDecomposed(vertsInTile, fInTile)
-	tileMesh.BBox[0][0] = tileBBox[0]
-	tileMesh.BBox[0][1] = tileBBox[1]
-	tileMesh.BBox[0][2] = tileBBox[2]
-	tileMesh.BBox[1][0] = tileBBox[3]
-	tileMesh.BBox[1][1] = tileBBox[4]
-	tileMesh.BBox[1][2] = tileBBox[5]
+	tileMesh.BBox[0][0] = tileBBox[0] - ctx
+	tileMesh.BBox[0][1] = tileBBox[1] - cty
+	tileMesh.BBox[0][2] = 0
+	tileMesh.BBox[1][0] = tileBBox[3] - ctx
+	tileMesh.BBox[1][1] = tileBBox[4] - cty
+	tileMesh.BBox[1][2] = 0
 
 	return tileMesh, nil
 }

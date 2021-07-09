eb32125581f822f8bc6694f2eedeaf475ac2e158
aninggo <aninggo@gmail.com>
2021-07-09 17:58:40 +0800

    update

--- tin.go
+++ tin.go
@@ -1,6 +1,3 @@
-// Copyright (c) 2017-present FlyWave, Inc. All Rights Reserved.
-// See License.txt for license information.
-
 package tin
 
 func GenerateTinMesh(raster *RasterDouble, maxError float64) *Mesh {
